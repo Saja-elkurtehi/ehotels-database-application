d4ce8be2388fb027d662caa3b11bab30e80134a9
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2025-03-16 19:02:54 -0400

    added starter files for project

---
+++ ehotels/src/main/java/ca/uottawa/csi2132/ehotels/EHotelsBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.uottawa.csi2132.ehotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHotelsBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHotelsBookingSystemApplication.class, args);
+	}
+
+}
---
+++ ehotels/src/test/java/ca/uottawa/csi2132/ehotels/EHotelsBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uottawa.csi2132.ehotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHotelsBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
